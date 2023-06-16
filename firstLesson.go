e911b91d9de8146db8ed1461a3a2dcac813e367b
Ananda Narayanan Udayakumar <ananduun@icloud.com>
2023-06-16 07:49:44 -0400

    first lesson

---
+++ firstLesson.go
@@ -0,0 +1,15 @@
+// Package - here we set the package name
+package main
+
+// Import - here we import the packages we need
+// fmt - package for formatting text
+// math - package for math operations
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Println("The square root of 4 is", math.Sqrt(4))
+}
